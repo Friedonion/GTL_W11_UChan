f402cfd6ac0621a14bf078e3f43cb4cae9d9b79e
FriedOnion <rkdwnd333@naver.com>
2025-05-19 23:28:22 +0900

    feat: 김우찬 텍스쳐 성공

--- EngineSIU/EngineSIU/Shaders/MeshParticlePixelShader.hlsl
+++ EngineSIU/EngineSIU/Shaders/MeshParticlePixelShader.hlsl
@@ -11,7 +11,6 @@ struct PS_INPUT
 
 float4 mainPS(PS_INPUT input) : SV_TARGET
 {
-    // 라이팅 계산 제거, 단순히 텍스처와 색상 곱하기
     float4 diffuseColor = MaterialTextures[0].Sample(MaterialSamplers[0], input.TexCoord);
     
     float4 finalColor = diffuseColor;
